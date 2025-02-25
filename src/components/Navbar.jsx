de57fbc99e9302c874b3061cfc12c2b151852957
Code-Nethranand <nethranand12345@gmail.com>
2025-02-25 20:10:13 +0530

    Header Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import {useRef} from 'react';
+import {useRef, useEffect} from 'react';
 import PropTypes from 'prop-types'
 
 const Navbar = ({ navOpen }) => {
 
     const lastActiveLink = useRef();
     const activeBox = useRef();
+
+    const initActionBox = () => {
+      activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+      activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+      activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+      activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
+      
+    }
+
+    useEffect(initActionBox, [])
+    window.addEventListener('resize', initActionBox)
+
+    const activeCurrentLink = (event) => {
+      lastActiveLink.current?.classList.remove('active')
+      event.target.classList.add('active')
+      lastActiveLink.current = event.target; 
+
+      activeBox.current.style.top = lastActiveLink.current.offsetTop + 'px';
+      activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px';
+      activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px';
+      activeBox.current.style.height = lastActiveLink.current.offsetHeight + 'px';
+    }
 
     const navItems = [  
         { 
@@ -40,7 +62,7 @@ const Navbar = ({ navOpen }) => {
         <nav className={'navbar ' + (navOpen ? 'active' : '')}>
             { 
                 navItems.map(( {label, link, className, ref}, key) => (
-                    <a href={link} className={className} ref={ref} key={key} onClick={null}>
+                    <a href={link} className={className} ref={ref} key={key} onClick={activeCurrentLink}>
                         {label}
                     </a>
                     

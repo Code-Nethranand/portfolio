b068ebea2c8c26f2ec107c156f84c5b6890857e4
Code-Nethranand <nethranand12345@gmail.com>
2025-02-26 20:24:39 +0530

    Skill Section Added

---
+++ src/components/SkillCard.jsx
@@ -0,0 +1,36 @@
+import PropType from 'prop-types'
+
+const SkillCard = ({
+    imgSrc,
+    label,
+    desc,
+    classes 
+}) => {
+  return (
+    <div className={'flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 translate-colors group ' + classes}>
+        <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 translate-colors">
+            <img 
+            src={imgSrc}
+            width={32}
+            height={32}
+            alt={label}
+            />
+        </figure>
+        <div>
+            <h3>{label}</h3>
+            <p className="text-zinc-400 text-sm">
+                {desc}
+            </p>
+        </div>
+    </div>
+  )
+}
+
+SkillCard.propTypes = {
+    imgSrc: PropType.string.isRequired,
+    label: PropType.string.isRequired,
+    desc: PropType.string.isRequired,
+    classes: PropType.string
+}
+
+export default SkillCard

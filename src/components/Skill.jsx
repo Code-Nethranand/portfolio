44e395db1227aaae7b3cc58becea62b893701b6f
Code-Nethranand <nethranand12345@gmail.com>
2025-06-26 00:01:03 +0530

    Skills Added

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,47 +1,168 @@
 import SkillCard from "./SkillCard" 
 
-
-
 const skillItem = [
     {
       imgSrc: '/images/figma.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968705.png',
       label: 'Figma',
       desc: 'Design tool'
     },
     {
       imgSrc: '/images/css3.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/732/732190.png',
       label: 'CSS',
       desc: 'User Interface'
     },
     {
       imgSrc: '/images/javascript.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png',
       label: 'JavaScript',
       desc: 'Interaction'
     },
     {
+      imgSrc: '/images/html5.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/732/732212.png',
+      label: 'HTML',
+      desc: 'Markup Language',
+      iconLink: 'https://www.flaticon.com/free-icons/html'
+    },
+    {
+      imgSrc: '/images/python.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png',
+      label: 'Python',
+      desc: 'Programming Language'
+    },
+    {
+      imgSrc: '/images/cpp.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/6132/6132222.png',
+      label: 'C/C++',
+      desc: 'Programming Language'
+    },
+    {
+      imgSrc: '/images/sql.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/4248/4248443.png',
+      label: 'SQL',
+      desc: 'Database Query'
+    },
+    {
+      imgSrc: '/images/linux.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/226/226772.png',
+      label: 'Linux',
+      desc: 'Operating System'
+    },
+    {
       imgSrc: '/images/nodejs.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/919/919825.png',
       label: 'NodeJS',
       desc: 'Web Server'
     },
     {
       imgSrc: '/images/expressjs.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/919/919831.png',
       label: 'ExpressJS',
       desc: 'Node Framework'
     },
     {
       imgSrc: '/images/mongodb.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/919/919836.png',
       label: 'MongoDB',
       desc: 'Database'
     },
     {
+      imgSrc: '/images/mysql.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/528/528260.png',
+      label: 'MySQL',
+      desc: 'Database'
+    },
+    {
+      imgSrc: '/images/postgresql.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968342.png',
+      label: 'PostgreSQL',
+      desc: 'Database'
+    },
+    {
       imgSrc: '/images/react.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/1126/1126012.png',
       label: 'React',
       desc: 'Framework'
     },
     {
+      imgSrc: '/images/nextjs.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968292.png',
+      label: 'Next.js',
+      desc: 'React Framework'
+    },
+    {
       imgSrc: '/images/tailwindcss.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/732/732212.png',
       label: 'TailwindCSS',
       desc: 'User Interface'
+    },
+    {
+      imgSrc: '/images/git.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/2111/2111288.png',
+      label: 'Git',
+      desc: 'Version Control'
+    },
+    {
+      imgSrc: '/images/github.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/733/733553.png',
+      label: 'GitHub',
+      desc: 'Code Hosting'
+    },
+    {
+      imgSrc: '/images/postman.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968705.png',
+      label: 'Postman',
+      desc: 'API Testing'
+    },
+    {
+      imgSrc: '/images/docker.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/919/919853.png',
+      label: 'Docker',
+      desc: 'Containerization'
+    },
+    {
+      imgSrc: '/images/powerbi.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968705.png',
+      label: 'Power BI',
+      desc: 'Data Analysis'
+    },
+    {
+      imgSrc: '/images/excel.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/732/732220.png',
+      label: 'Excel',
+      desc: 'Data Analysis'
+    },
+    {
+      imgSrc: '/images/pandas.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png',
+      label: 'Pandas',
+      desc: 'Data Analysis'
+    },
+    {
+      imgSrc: '/images/numpy.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png',
+      label: 'NumPy',
+      desc: 'Data Analysis'
+    },
+    {
+      imgSrc: '/images/matplotlib.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/5968/5968350.png',
+      label: 'Matplotlib',
+      desc: 'Data Visualization'
+    },
+    {
+      imgSrc: '/images/aws.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/873/873120.png',
+      label: 'AWS',
+      desc: 'Cloud Services'
+    },
+    {
+      imgSrc: '/images/gcp.svg',
+      fallbackImg: 'https://cdn-icons-png.flaticon.com/512/6124/6124995.png',
+      label: 'Google Cloud',
+      desc: 'Cloud Services'
     },
 ]
 
@@ -58,19 +179,21 @@ const Skill = () => {
             </p>
 
             <div className="grid gap-3 grid-cols-[repeat(auto-fill,_minmax(250px,_1fr))]">
-                {
-                    skillItem.map(({ imgSrc, label, desc},key) =>
-                    (
-                        <SkillCard
-                            key={key}
-                            imgSrc={imgSrc}
-                            label={label}
-                            desc={desc}
-                            classes="reveal-up"
-                        />
-                    ))
-                }
-            </div>
+    {
+        skillItem.map(({ imgSrc, label, desc, fallbackImg, iconLink }, key) =>
+        (
+            <SkillCard
+                key={key}
+                imgSrc={imgSrc}
+                label={label}
+                desc={desc}
+                fallbackImg={fallbackImg}
+                iconLink={iconLink}
+                classes="reveal-up"
+            />
+        ))
+    }
+</div>
         </div>
     </section>
   )
--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -4,17 +4,40 @@ const SkillCard = ({
     imgSrc,
     label,
     desc,
-    classes 
+    classes,
+    iconLink,
+    fallbackImg
 }) => {
+  // Handler to swap to fallback image if local image fails
+  const handleImgError = (e) => {
+    if (fallbackImg && e.target.src !== fallbackImg) {
+      e.target.onerror = null;
+      e.target.src = fallbackImg;
+    }
+  };
+
   return (
     <div className={'flex items-center gap-3 ring-2 ring-inset ring-zinc-50/10 rounded-2xl p-3 hover:bg-zinc-800 translate-colors group ' + classes}>
         <figure className="bg-zinc-700/50 rounded-lg overflow-hidden w-12 h-12 p-2 group-hover:bg-zinc-900 translate-colors">
-            <img 
-            src={imgSrc}
-            width={32}
-            height={32}
-            alt={label}
-            />
+            {iconLink ? (
+                <a href={iconLink} target="_blank" rel="noopener noreferrer">
+                    <img 
+                        src={imgSrc}
+                        width={32}
+                        height={32}
+                        alt={label}
+                        onError={handleImgError}
+                    />
+                </a>
+            ) : (
+                <img 
+                    src={imgSrc}
+                    width={32}
+                    height={32}
+                    alt={label}
+                    onError={handleImgError}
+                />
+            )}
         </figure>
         <div>
             <h3>{label}</h3>
@@ -30,7 +53,9 @@ SkillCard.propTypes = {
     imgSrc: PropType.string.isRequired,
     label: PropType.string.isRequired,
     desc: PropType.string.isRequired,
-    classes: PropType.string
+    classes: PropType.string,
+    iconLink: PropType.string,
+    fallbackImg: PropType.string
 }
 
 export default SkillCard
